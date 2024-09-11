39445fd9379ebffa4cdeadff24f4a208cb46b7e1
javier <cristianchaile934@gmail.com>
2024-09-11 15:37:23 -0300

    agregar array

---
+++ ejercicio5.js
@@ -0,0 +1,39 @@
+class CuentaBancaria {
+    constructor(saldoInicial = 0) {
+        this.saldo = saldoInicial;
+    }
+
+    depositar(cantidad) {
+        if (cantidad > 0) {
+            this.saldo += cantidad;
+            console.log(`Depositado: $${cantidad.toFixed(2)}. Saldo actual: $${this.saldo.toFixed(2)}`);
+        } else {
+            console.log("La cantidad a depositar debe ser positiva.");
+        }
+    }
+
+    retirar(cantidad) {
+        if (cantidad > 0) {
+            if (cantidad <= this.saldo) {
+                this.saldo -= cantidad;
+                console.log(`Retirado: $${cantidad.toFixed(2)}. Saldo actual: $${this.saldo.toFixed(2)}`);
+            } else {
+                console.log("Fondos insuficientes para realizar el retiro.");
+            }
+        } else {
+            console.log("La cantidad a retirar debe ser positiva.");
+        }
+    }
+}
+
+// Crear una instancia de CuentaBancaria con un saldo inicial de $1000
+const cuenta = new CuentaBancaria(1000);
+
+// Realizar varias operaciones
+cuenta.depositar(500);   // Depositar $500
+cuenta.retirar(200);    // Retirar $200
+cuenta.depositar(300);   // Depositar $300
+cuenta.retirar(1000);   // Intentar retirar $1000 (debería fallar)
+
+// Mostrar el saldo final
+console.log(`Saldo final: $${cuenta.saldo.toFixed(2)}`);
---
+++ ejercicio6.js
@@ -0,0 +1,37 @@
+// const arrayA = [1,2,3,4,5] forma normal
+// for(let  i=0;i < 5 ;i++){
+    // console.log(arrayA)}
+// console.log (arrayA[0])
+
+// ----
+// const arrayA = [1,2,3,4,5,6,7,8,9,10,11,121,123,34,56,7,8,,978,9,789,789,7,9,89,78,979,7,878,98,7,8]
+// for(let  i=0;i < arrayA.length ;i++){
+    // console.log(arrayA)
+// }
+// console.log(arrayA[0])
+// console.log(arrayA.length)
+// console.log("el array cuanta con",arrayA.length) fprma para saber la longitud de array
+// -----
+// const arrayB =["manzana,banana,naranja"]
+// const arrayC =["tomate,lechuga,abano"]
+// for(let i=0;i<arrayC.length;i++){
+    // arrayB.push(arrayC[i])
+// }
+// console.log(arrayB)
+// -----
+//  const arrayB =["manzana,banana,naranja"]
+//  const arrayC =["tomate,lechuga,abano"]
+ 
+//  const ensalada= arrayB.concat(arrayC)
+//  console.log(ensalada)
+
+// {const listadenombres =[]
+// let cantidad = 5
+// do{
+//     let entrada =prompt("ingresar nombre")
+//     listadenombres.push(entrada.toUpperCase())
+//     console.log(listadenombres.length)
+// }while(listadenombres.length != cantidad)
+//     const nuevalista = listadenombres.concat([magali,julieta])
+    // alert(nuevalista.join("\n"))}
+
