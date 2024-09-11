08760997fb43858a5f9d8ca8534ae2b2879bd461
javier <cristianchaile934@gmail.com>
2024-09-11 16:18:35 -0300

    modificar

---
+++ ejercicio7.js
@@ -0,0 +1,23 @@
+// EJERCICIO1
+// const nombres = []
+// const cantidadmaxima= 10
+// while(nombres.length <=cantidadmaxima){
+//     const  nombre = prompt("ingrese un nombre")
+//     nombres.push(nombre)
+// }
+// console.log("antes de elminar el ultimo",nombres)
+// nombres.pop
+// console.log("despues de eleminar el ultimo",nombres)
+
+
+// EJERCICIO2   
+// const frutas = ["manzana,frutilla,banana,pera,uva"]
+// for(let i=0;i<frutas.length;i++){
+//     console.log (frutas[i])
+// }
+// alert(frutas)
+// frutas.shift()
+// alert(frutas)
+// let nuevafruta = prompt("ingresa la fruta")
+// frutas.unshift(nuevafruta)
+// alert(frutas)
